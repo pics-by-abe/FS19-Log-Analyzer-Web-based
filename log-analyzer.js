e5ffa6b0e051872565fd18d3c93c78a54819c3bf
pics-by-abe <abe@bayern-agrar.me>
2020-11-11 21:50:57 +0100

    header done

---
+++ log-analyzer.js
@@ -0,0 +1,12 @@
+  document.getElementById("selLog").click();
+  const fileSelector = document.getElementById('file-selector');
+  
+  fileSelector.addEventListener('change', (event) => {
+  
+    const fileList = event.target.files;
+  
+    console.log(fileList);
+  
+  });
+
+
