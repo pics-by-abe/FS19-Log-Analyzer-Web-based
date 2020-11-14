8c9129e318641f729ab3bd86bbb707e53c115d92
pics-by-abe <abe@bayern-agrar.me>
2020-11-14 21:23:10 +0100

    verzweifelung

--- log-analyzer.js
+++ log-analyzer.js
@@ -1,10 +1,16 @@
+/* Select File -> real to virtual button*/
 const realFileBtn = document.getElementById("myfile");
 const customBtn = document.getElementById("selLogFilebtn");
 const customTxt = document.getElementById("realText");
 
+const fs = require("fs");
+
+
 customBtn.addEventListener("click", function() {
   realFileBtn.click();
-});
+
+
+  });
 
 realFileBtn.addEventListener("change", function() {
   if (realFileBtn.value) {
@@ -15,3 +21,22 @@ realFileBtn.addEventListener("change", function() {
     customTxt.innerHTML = "No file chosen, yet.";
   }
 });
+
+
+
+
+
+try {
+  // read contents of the file
+  const data = fs.readFileSync('file.txt', 'UTF-8');
+
+  // split the contents by new line
+  const lines = data.split(/\r?\n/);
+
+  // print all lines
+  lines.forEach((line) => {
+      console.log(line);
+  });
+} catch (err) {
+  console.error(err);
+}
